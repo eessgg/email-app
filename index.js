c448b538243ac835e8f84cdb81959765ac29a57a
eessgg <estergomesdesign@gmail.com>
2020-06-08 14:53:34 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/greetings', (req, res) => {
+  res.json({message: 'ok'});
+});
+
+app.get("/greetings", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+
+
+
+app.listen(PORT, () => console.log(PORT));
